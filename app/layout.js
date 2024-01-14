8d4c345e7726153b9a2d124363cca44705d34002
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-14 10:20:10 +0200

    solve problem of white space in right of the page (#rootReactVNB)

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="rtl">
       <body>
-        <>
-          <NavBar />
-          {children}
-        </>
+        <NavBar />
+        {children}
       </body>
     </html>
   );
