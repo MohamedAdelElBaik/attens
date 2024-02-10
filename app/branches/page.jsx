a434a191e506307f02552393c3ecfd4a61fa7022
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-10 23:59:39 +0200

    COMPONENT : info card (not finish)

--- app/branches/page.jsx
+++ app/branches/page.jsx
@@ -3,11 +3,14 @@ import NotificationBtn from '@/components/NotificationBtn';
 import Search from '@/components/Search';
 import UserImage from '@/components/UserImage';
 import style from '@/app/branches/page.module.css';
+import Image from 'next/image';
 
 function page() {
   return (
     <div className={`${style.branches} gap-5 m-4`}>
-      <div className={`${style.box1} bg-white m-1 rounded-md w-full`}>1</div>
+      <div className={`${style.box1} bg-primary-300 m-1 rounded-md w-full`}>
+        <InfoCard />
+      </div>
       <div className={`${style.box2} bg-white m-1 rounded-md w-full`}>2</div>
       <div className={`${style.box3} bg-white m-1 rounded-md w-full`}>3</div>
       <div className={`${style.box4} bg-white m-1 rounded-md w-full`}>4</div>
@@ -17,3 +20,18 @@ function page() {
 }
 
 export default page;
+
+function InfoCard() {
+  return (
+    <div className="flex gap-2">
+      <div>
+        <h2 className="text-3xl font-medium">تنظيم فروعك يبدأ من هنا !</h2>
+        <p className="text-base">
+          قم بسهولة اضافة فرع جديد تحديد الموقع وساعات العمل او قم بتعديل الأفرع
+          الموجوة مسبقا واحكم برموز الحضور والموظفين الموجودين. تعلم المزيد...
+        </p>
+      </div>
+      <Image src="/maxmizeIcon.png" width={250} height={250} alt="ss"></Image>
+    </div>
+  );
+}
