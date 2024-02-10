34f20e7ae155a9d6c1c09cf676199e2cc6b643ef
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-10 23:41:05 +0200

    CHANGE : Header component from page to layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import NavBar from '@/components/NavBar';
 import './globals.css';
+import BranchSelect from '@/components/BranchSelect';
+import WorkShiftSelect from '@/components/WorkShiftSelect';
+import Search from '@/components/Search';
+import MessageBtn from '@/components/MessageBtn';
+import NotificationBtn from '@/components/NotificationBtn';
+import UserImage from '@/components/UserImage';
+import { SelectProvider } from '@/contexts/SelectContext';
 
 export const metadata = {
   title: 'Attens',
@@ -12,10 +19,38 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="rtl">
-      <body>
-        <NavBar />
-        {children}
+      <body className="">
+        <SelectProvider>
+          <NavBar />
+
+          <div className="hidden">
+            <Header>
+              <Section>
+                <BranchSelect />
+                <WorkShiftSelect />
+              </Section>
+              <Section>
+                <Search />
+                <MessageBtn />
+                <NotificationBtn />
+                <UserImage
+                  className={'cursor-pointer mr-10'}
+                  src={'/user.webp'}
+                  size={50}
+                />
+              </Section>
+            </Header>
+          </div>
+
+          <div className="block">{children}</div>
+        </SelectProvider>
       </body>
     </html>
   );
+}
+function Header({ children }) {
+  return <header className="flex justify-between haa-10">{children}</header>;
+}
+function Section({ children }) {
+  return <div className="flex gap-4">{children}</div>;
 }
--- app/page.js
+++ app/page.js
@@ -8,46 +8,20 @@ import DayAttendance from '@/components/dashboard/DayAttendance';
 import Hello from '@/components/dashboard/Hello';
 import Map from '@/components/dashboard/Map';
 import QRCode from '@/components/dashboard/QRCode';
-import { SelectProvider } from '@/contexts/SelectContext';
 
 export default function Home() {
   return (
     <div className="my-6 mx-10 w-full">
-      <SelectProvider>
-        <Header>
-          <Section>
-            <BranchSelect />
-            <WorkShiftSelect />
-          </Section>
-          <Section>
-            <Search />
-            <MessageBtn />
-            <NotificationBtn />
-            <UserImage
-              className={'cursor-pointer mr-10'}
-              src={'/user.webp'}
-              size={50}
-            />
-          </Section>
-        </Header>
-
-        <Main>
-          <Hello className="w-96" />
-          {/* <QRCode className="w-96" /> */}
-          <Map />
-          {/* <DayAttendance /> */}
-        </Main>
-      </SelectProvider>
+      <Main>
+        <Hello className="w-96" />
+        {/* <QRCode className="w-96" /> */}
+        <Map />
+        {/* <DayAttendance /> */}
+      </Main>
     </div>
   );
 }
 
-function Header({ children }) {
-  return <header className="flex justify-between ">{children}</header>;
-}
-function Section({ children }) {
-  return <div className="flex gap-4">{children}</div>;
-}
 function Main({ children }) {
   return <main className="mt-6">{children}</main>;
 }
